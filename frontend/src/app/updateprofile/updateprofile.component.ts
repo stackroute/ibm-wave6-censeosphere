9fddbfefc59ac2b48f81d9c7b58085a889fbd092
ganga <ganga.janvi03@gmail.com>
2019-07-10 11:11:31 +0530

    reviewer updateprofile updated

--- frontend/src/app/updateprofile/updateprofile.component.ts
+++ frontend/src/app/updateprofile/updateprofile.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
-
-
 import { LandingpageService } from '../landingpage.service';
+import { SearchForReviewService } from '../search-for-review.service';
 
 
 @Component({
@@ -14,13 +12,19 @@ import { LandingpageService } from '../landingpage.service';
 export class UpdateprofileComponent implements OnInit {
 
   products=[];
-  constructor(private router:Router,private landingpageservice:LandingpageService) { }
+  reviews=[];
+  constructor(private router:Router,private landingpageservice:LandingpageService,private searchforreview:SearchForReviewService) { }
 
   ngOnInit() {
 
     this.landingpageservice.getAllProducts().subscribe((data:any) => {
       console.log(data);
       this.products=data;
+    })
+
+    this.searchforreview.getAllReviews().subscribe((data:any) =>{
+      console.log(data);
+      this.reviews=data;
     })
   }
   update()
