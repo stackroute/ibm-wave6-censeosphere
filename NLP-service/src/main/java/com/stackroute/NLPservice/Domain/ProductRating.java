2d911c3929c989205f6880bf62d2be9cc8957818
monieeesha <monikadri13@gmail.com>
2019-07-15 15:48:09 +0530

    nlp-service completed

---
+++ NLP-service/src/main/java/com/stackroute/NLPservice/Domain/ProductRating.java
@@ -0,0 +1,22 @@
+package com.stackroute.NLPservice.Domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "productrating")
+public class ProductRating {
+
+    @Id
+    String productName;
+    int rating=0;
+}
+
--- review-service/src/main/java/com/stackroute/review/service/ReviewServiceImpl.java
+++ review-service/src/main/java/com/stackroute/review/service/ReviewServiceImpl.java
@@ -1,8 +1,11 @@
 package com.stackroute.review.service;
 
 import com.stackroute.review.domain.Review;
+import com.stackroute.review.dto.ReviewDTO;
 import com.stackroute.review.repository.ReviewRepository;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,17 +16,31 @@ public class ReviewServiceImpl implements ReviewService {
     @Autowired
     private ReviewRepository reviewRepository;
 
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Value("${stackroute.rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${stackroute.rabbitmq.routingkeythree}")
+    private String routingkeythree;
+
     @Autowired
     public ReviewServiceImpl(ReviewRepository reviewRepository)
     {
         this.reviewRepository=reviewRepository;
     }
+
     @Override
     public Review addReview(Review review) {
 
         //rabbitmq queue, exchange,routingkey generation
         Review savedReview = reviewRepository.save(review);
+        ReviewDTO reviewDTO=new ReviewDTO(review.getProductName(),review.getReviewDescription());
+        sendRating(reviewDTO);
         return savedReview;
+
     }
 
     @Override
@@ -31,6 +48,15 @@ public class ReviewServiceImpl implements ReviewService {
         return reviewRepository.findAll();
     }
 
+
+    @Override
+    public void sendRating(ReviewDTO reviewDTO)
+    {
+
+        rabbitTemplate.convertAndSend(exchange, routingkeythree, reviewDTO);
+        System.out.println("Send msg = " + reviewDTO.toString());
+
+    }
 
 
 }
