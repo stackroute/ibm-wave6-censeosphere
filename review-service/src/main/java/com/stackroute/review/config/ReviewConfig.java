ac473c6e78bb6fe5792a5f69a8e799e0160bef40
ganga <ganga.janvi03@gmail.com>
2019-07-15 15:40:02 +0530

    stepper form with validation and backend connectivity

--- review-service/src/main/java/com/stackroute/review/config/ReviewConfig.java
+++ review-service/src/main/java/com/stackroute/review/config/ReviewConfig.java
@@ -1,35 +1,35 @@
-package com.stackroute.review.config;
-
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ReviewConfig {
-
-    @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
-    @Bean
-    ConnectionFactory connectionFactory(){
-        //we want connection to be stable,so that we needn't close or open connection
-        CachingConnectionFactory cachingConnectionFactory =new CachingConnectionFactory("localhost");
-        cachingConnectionFactory.setUsername("guest");
-        cachingConnectionFactory.setPassword("guest");
-        return cachingConnectionFactory;
-    }
-
-
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(jsonMessageConverter());
-        return rabbitTemplate;
-    }
-
-}
+//package com.stackroute.review.config;
+//
+//import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+//import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+//import org.springframework.amqp.support.converter.MessageConverter;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ReviewConfig {
+//
+//    @Bean
+//    public MessageConverter jsonMessageConverter() {
+//        return new Jackson2JsonMessageConverter();
+//    }
+//    @Bean
+//    ConnectionFactory connectionFactory(){
+//        //we want connection to be stable,so that we needn't close or open connection
+//        CachingConnectionFactory cachingConnectionFactory =new CachingConnectionFactory("localhost");
+//        cachingConnectionFactory.setUsername("guest");
+//        cachingConnectionFactory.setPassword("guest");
+//        return cachingConnectionFactory;
+//    }
+//
+//
+//    @Bean
+//    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+//        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+//        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+//        return rabbitTemplate;
+//    }
+//
+//}
