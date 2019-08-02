fdf189135917904c05bb3a94573dc273c5665890
aiswarya <aiswaryageorge1996@gmail.com>
2019-08-02 15:36:04 +0530

    implemented grafana

--- user-authentication-service/reviewer-profile-service/src/main/java/com/stackroute/reviewerprofile/repository/ReviewerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stackroute.reviewerprofile.repository;
-
-import com.stackroute.reviewerprofile.domain.Reviewer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ReviewerRepository extends MongoRepository<Reviewer,String>
-{
-}
