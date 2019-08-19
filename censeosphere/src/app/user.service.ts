822cbd0b0a044f5a6beb9f30e7b03a9e56340957
MeghnaW19 <meghnanvt70@gmail.com>
2019-08-19 17:06:14 +0530

    Update user.service.ts

--- censeosphere/src/app/user.service.ts
+++ censeosphere/src/app/user.service.ts
@@ -14,16 +14,16 @@ headers= new HttpHeaders({'Access-Control-Allow-Origin' : '*'})
   constructor(private http:HttpClient) { }
 
   getAllUsers():any{
-    return this.http.get("http://140.238.167.40:8083/user-login-service/api/user/getuser");
+    return this.http.get("http://140.238.162.66:8083/user-login-service/api/user/getuser");
   }
 
   saveUser(user:User){
     console.log("sadgui"+user)
-    return this.http.post<User>("http://140.238.167.40:8083/user-login-service/api/user/users",user);
+    return this.http.post<User>("http://140.238.162.66:8083/user-login-service/api/user/users",user);
   }
 
   login(user:User){
-    return this.http.post<User>("http://140.238.167.40:8083/user-login-service/api/user/user",user);
+    return this.http.post<User>("http://140.238.162.66:8083/user-login-service/api/user/user",user);
 
   }
 }
