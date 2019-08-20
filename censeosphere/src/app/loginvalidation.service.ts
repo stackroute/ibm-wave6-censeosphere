70fef7b8997fd4353eae961a505d7c072d66b109
sushmitha007 <42641604+sushmitha007@users.noreply.github.com>
2019-08-20 14:42:08 +0530

    Update loginvalidation.service.ts

--- censeosphere/src/app/loginvalidation.service.ts
+++ censeosphere/src/app/loginvalidation.service.ts
@@ -14,7 +14,7 @@ headers= new HttpHeaders({'Access-Control-Allow-Origin' : '*'})
 
   login(auth:Authentication){
     console.log("in service "+JSON.stringify(auth));
-    return this.http.post("http://13.126.244.58:8083/user-login-service/api/user/user",auth);
+    return this.http.post("http://140.238.167.169:8083/user-login-service/api/user/user",auth);
 
   }
 }
