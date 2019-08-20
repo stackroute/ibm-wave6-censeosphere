02c1296b99eca579840325dabc0af043ab65302e
sushmitha007 <42641604+sushmitha007@users.noreply.github.com>
2019-08-20 14:45:51 +0530

    Update profile.service.ts

--- censeosphere/src/app/profile.service.ts
+++ censeosphere/src/app/profile.service.ts
@@ -14,11 +14,11 @@ export class ProfileService {
 
   saveReviewer(reviewer:Reviewer){
     console.log("hello there ", reviewer);
-   return this.http.post<Reviewer>("http://13.126.244.58:8083/reviewer-profile-service/api/v1/reviewer",reviewer);
+   return this.http.post<Reviewer>("http://140.238.167.169:8083/reviewer-profile-service/api/v1/reviewer",reviewer);
   }
   saveProductowner(reviewer:Reviewer)
   {
     console.log("helloooooooo "+reviewer);
-    return this.http.post<Reviewer>("http://13.126.244.58:8083/product-owner-service/api/v1/product",reviewer);  
+    return this.http.post<Reviewer>("http://140.238.167.169:8083/product-owner-service/api/v1/product",reviewer);  
   }
 }
